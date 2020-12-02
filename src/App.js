964c0cb58ff2a49dc4bf1e30b8e6546c1ce7e756
anton-ua <antonyua23@gmail.com>
2020-12-02 23:27:04 +0200

    add input component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import Header from "./Components/Header/Header";
+import styles from "./App.module.css";
+import InputForm from "./Components/InputForm/InputForm";
 
-function App() {
-  return <div className='App'></div>;
-}
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <header>
+        <div className={styles.container}>
+          <Header />
+        </div>
+      </header>
+      <body>
+        <div className={styles.container}>
+          <InputForm />
+        </div>
+      </body>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+const Header = () => <h1 className={styles.title}>tracker</h1>;
+
+export default Header;
